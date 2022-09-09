00d62c774991c58ed6e444b26b6860bb0b77039f
kanonkc <kanonkce@gmail.com>
2022-09-09 21:47:16 +0700

    Rename folder + Database fir st migrations

--- api/models.py
+++ api/models.py
@@ -1,6 +1,18 @@
-from pyexpat import model
 from django.db import models
 
-class Item(model.Model):
+class Item(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=50,default="")
+    lastname = models.CharField(max_length=50,default="")
+    username = models.CharField(max_length=50,unique=True)
+    password = models.CharField(max_length=128,default=None)
+    email = models.CharField(max_length=50,default=None)
+    year_of_birth = models.IntegerField(blank=True,default=None)
+    description = models.CharField(max_length=50,blank=True,default=None)
+    is_verified = models.BooleanField(default=False)
+    profile_picture = models.CharField(max_length=1000,blank=True,default=None)
+    user_status = models.CharField(max_length=10,default=None)
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
+from . import false_views
+from .views import user
 
 urlpatterns = [
-    path('/',views.getData),
-    path('/register',views.registerAccount),
-    path('/account/<str:username>',views.personalAccount),
-    path('/login',views.loginAccount),
+    path('',false_views.getData),
+    path('register',user.register)
 ]
