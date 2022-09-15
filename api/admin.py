6d6c12bc731bf2186e3edac60137f4147f08be57
kanonkc <kanonkce@gmail.com>
2022-09-15 17:27:22 +0700

    Add Account, School to Admin Page

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from api.models import Account
+from api.models import Account,School
 
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ['account_id','username','firstname','lastname','email','is_verified','user_status']
+    list_display = ['account_id','username','firstname','lastname','email','is_verified','is_deleted']
     search_fields = ['username','firstname','lastname']
-    list_filter = ['is_verified','user_status']
+    list_filter = ['is_verified','is_deleted']
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ['school_id','owner_id','name','address']
 
 admin.site.register(Account,AccountAdmin)
+admin.site.register(School,SchoolAdmin)
