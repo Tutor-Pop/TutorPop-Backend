ec4171609c205201fb2241dc4106c9401752fbdb
winnone <chananan254555@gmail.com>
2022-09-23 16:37:33 +0700

    เติม s school ใน path

--- api/urls.py
+++ api/urls.py
@@ -11,15 +11,14 @@ urlpatterns = [
     path('account/<int:id>/school', account.create_school),
     #--- 3. Course ---#
     # path('course/<int:course_id>',course.get_course),
-    path('school/<int:school_id>/course', course.create_course),
+    path('schools/<int:school_id>/course', course.create_course),
     #--- 5. Room ---#
-    path('school/<int:school_id>/room', room.create_room),
-    path('school/<int:school_id>/rooms', room.get_all_room_in_school),
-    path('school/<int:school_id>/room/<int:room_id>', room.get_update_room),
+    path('schools/<int:school_id>/room', room.create_room),
+    path('schools/<int:school_id>/rooms', room.get_all_room_in_school),
+    path('schools/<int:school_id>/room/<int:room_id>', room.get_update_room),
     #--- 6. Request ---#
     path('requests', request.get_create_request),
     path('requests/<int:req_id>', request.get_del_update_request),
-
     #--- Demo ---#
     path('test', test.get_acounts),
     path('test/<int:id>/edit', test.edit_account),
