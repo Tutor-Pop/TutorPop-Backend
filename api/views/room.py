d0a154880c03aaa5a4d11cfc32d639c41e91eddb
kanonkc <kanonkce@gmail.com>
2022-09-26 13:19:13 +0700

    TP-97 - Room API

--- api/views/room.py
+++ api/views/room.py
@@ -6,22 +6,31 @@ from ..constants.method import GET,POST,PUT,DELETE
 from ..models import School,SchoolRooms
 from rest_framework import status
 
-@api_view([POST])
-def create_room(request,school_id:int):
-    school = School.objects.get(school_id=school_id)
-    # room = SchoolRooms.objects.get(school_id=school,room_name=request.data['room_name'])
-    # return Response({"message":"Room already exists!"},status=status.HTTP_409_CONFLICT)
-    room = SchoolRooms(
-        school = school,
-        room_name = request.data['room_name'],
-        maximum_seat = request.data['maximum_seat']
-    )
-    room.save()
-    return Response({'result':JSONParserOne(room)},status=status.HTTP_201_CREATED)
-    
+@api_view([GET,POST])
+def create_getall_room(request,school_id:int):
+    if request.method == GET:
+        school = School.objects.get(school_id=school_id)
+        room = SchoolRooms.objects.filter(school_id=school)
+        max_seat = request.GET.get("max_seat",-1)
+        if max_seat != -1: room = room.filter(maximum_seat__lte=max_seat)
 
+        result = JSONParser(room)
+        count = len(result)
+        return Response({'count':count,'result':result},status=status.HTTP_200_OK)
+    elif request.method == POST:
+        school = School.objects.get(school_id=school_id)
+        # room = SchoolRooms.objects.get(school_id=school,room_name=request.data['room_name'])
+        # return Response({"message":"Room already exists!"},status=status.HTTP_409_CONFLICT)
+        room = SchoolRooms(
+            school = school,
+            room_name = request.data['room_name'],
+            maximum_seat = request.data['maximum_seat']
+        )
+        room.save()
+        return Response({'result':JSONParserOne(room)},status=status.HTTP_201_CREATED)
+    
 @api_view([GET,PUT,DELETE])
-def get_update_room(request,school_id: int,room_id: int):
+def get_update_delete_room(request,school_id: int,room_id: int):
     try:
         school = School.objects.get(school_id=school_id)
         room = SchoolRooms.objects.get(school_id=school,room_id=room_id)
@@ -37,6 +46,7 @@ def get_update_room(request,school_id: int,room_id: int):
         
         elif request.method == DELETE:
             room.is_deleted = True
+            room.save()
             return Response(status=status.HTTP_204_OK)
         
         return Response({'result':JSONParserOne(room)},status=status.HTTP_200_OK)
@@ -45,14 +55,3 @@ def get_update_room(request,school_id: int,room_id: int):
         return Response({"message": "Room doesn't not exists!"},status=status.HTTP_404_NOT_FOUND)
     except School.DoesNotExist:
         return Response({"message": "School doesn't not exists!"},status=status.HTTP_404_NOT_FOUND)
-
-@api_view([GET])
-def get_all_room_in_school(request,school_id:int):
-    school = School.objects.get(school_id=school_id)
-    room = SchoolRooms.objects.filter(school_id=school)
-    max_seat = request.GET.get("max_seat",-1)
-    if max_seat != -1: room = room.filter(maximum_seat__lte=max_seat)
-
-    result = JSONParser(room)
-    count = len(result)
-    return Response({'count':count,'result':result},status=status.HTTP_200_OK)
