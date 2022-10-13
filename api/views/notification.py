6ab2bf216039e4f1204825c41b4458b605c04666
n-tk11 <tulsorawit@hotmail.com>
2022-10-13 22:00:29 +0700

    modity noti create time to use django timezone

--- api/views/notification.py
+++ api/views/notification.py
@@ -6,37 +6,50 @@ from ..constants.method import GET, POST
 from ..models import Account, Notification
 from rest_framework import status
 import datetime
+from django.utils import timezone
+
 
 @api_view([POST])
 def create_notification(request):
-    now = datetime.datetime.now()
-    account = Account.objects.get(account_id=request.data["account_id"])    
+    account = Account.objects.get(account_id=request.data["account_id"])
     notification = Notification(
-        account_id = account,
-        message_noti = request.data["message_noti"],
-        create_time = f"{now.year}-{now.month}-{now.day} {now.hour}:{now.minute}",
-        expire_date = request.data["expire_date"],
+        account_id=account,
+        message_noti=request.data["message_noti"],
+        create_time=timezone.now(),
+        expire_date=request.data["expire_date"],
     )
     notification.save()
     result = JSONParserOne(notification)
-    return Response ({"message": result}, status=status.HTTP_200_OK)
+    return Response({"message": result}, status=status.HTTP_200_OK)
+
 
 @api_view([GET])
 def get_all_notification(request, account_ID: int):
     now = datetime.datetime.now()
-    notificationData = JSONParser(Notification.objects.filter(account_id = account_ID))
+    notificationData = JSONParser(Notification.objects.filter(account_id=account_ID))
     count = len(notificationData)
-    countDict = {"count":count}
-    return Response ({"count":countDict["count"], "messages": notificationData}, status=status.HTTP_200_OK)
+    countDict = {"count": count}
+    return Response(
+        {"count": countDict["count"], "messages": notificationData},
+        status=status.HTTP_200_OK,
+    )
+
 
 @api_view([GET])
 def get_notexpire_notification(request, account_ID: int):
     now = datetime.datetime.now()
-    current = str(now.year)+"-"+str(now.month)+"-"+str(now.day)
-    current = datetime.datetime.strptime(current, '%Y-%m-%d')
+    current = str(now.year) + "-" + str(now.month) + "-" + str(now.day)
+    current = datetime.datetime.strptime(current, "%Y-%m-%d")
     # print(type(current.date()))
     # print(datetime.date.today())
-    notificationData = JSONParser(Notification.objects.filter(account_id = account_ID, expire_date__gte = current.date()))
+    notificationData = JSONParser(
+        Notification.objects.filter(
+            account_id=account_ID, expire_date__gte=current.date()
+        )
+    )
     count = len(notificationData)
-    countDict = {"count":count}
-    return Response ({"count":countDict["count"], "messages": notificationData}, status=status.HTTP_200_OK)
+    countDict = {"count": count}
+    return Response(
+        {"count": countDict["count"], "messages": notificationData},
+        status=status.HTTP_200_OK,
+    )
